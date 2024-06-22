f0f57891e9e7b60c25d82d720529de06d7d8613f
Phuzamanzegiza <francis.ngema@live.com>
2024-06-22 18:05:46 +0200

    created website

---
+++ portfolio.R
@@ -0,0 +1,26 @@
+
+# Install necessary libraries
+#install.packages("remotes")
+
+#install.packages("blogdown")
+library(blogdown)
+
+# Load the remotes package
+library(remotes)
+
+# Install the portfoliodown package from GitHub
+remotes::install_github("business-science/portfoliodown")
+
+#Create a new portfoliodown site
+portfoliodown::new_portfolio_site(
+    "docs/my_portfolio_website",
+    theme = "raditian"                           
+)
+
+
+# Navigate to the directory containing the site
+setwd("docs/my_portfolio_website")
+
+
+# Render the entire site
+rmarkdown::render_site()
